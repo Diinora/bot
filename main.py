b3ff23ff6fdebd7e556caebf6356b28c3e5bff9c
Diinora <dinoram1711@gmail.com>
2026-06-26 15:14:23 +0300

    books

---
+++ main.py
@@ -0,0 +1,31 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+TOKEN = os.getenv("API")
+bot=Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer("Assalawma aleykum 😊")
+
+@dp.message(Command("help"))
+async def help(message: Message):
+    await message.answer("Qanday jardem kerek?")
+
+
+async def main():
+
+    print("bot iske qosildi ✅")
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
